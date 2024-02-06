219bfbba0ae75472ecf70d381578b04135f718bf
DhananjayaKumara99 <it21184208@my.sliit.lk>
2024-02-06 19:44:51 +0530

    change app.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Test from "./Test";
 import "./app.scss";
 import Contact from "./components/contact/Contact";
-import Cursor from "./components/cursor/Cursor";
+
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
